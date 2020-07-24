6908efb511c349a425598eac9f99781b83afd770
Douglas Wille <willedouglas10@gmail.com>
2020-07-24 18:16:41 -0300

    fix: updated origin paths with production and staging

--- src/config/origin.js
+++ src/config/origin.js
@@ -4,10 +4,25 @@ const dotenv = require('./dotenv');
 
 dotenv();
 
-const { FRONT_WEB_URL } = process.env;
+const getOriginUrl = () => {
+  let originUrl = 'http://localhost:8081';
+
+  const isProduction = process.env.NODE_ENV = 'production';
+  const isStaging = process.env.NODE_ENV = 'staging';
+
+  if (isProduction) {
+    originUrl = 'https://adfinance-register.a55.tech';
+  }
+
+  if (isStaging) {
+    originUrl = 'https://adfinance-register-staging.a55.tech';
+  }
+
+  return originUrl;
+}
 
 const origin = {
-  origin: FRONT_WEB_URL,
+  origin: getOriginUrl(),
   credentials: true,
 };
 
