98678b95df2c04d4514cb77cc2dfef7fa101f1d1
Douglas Wille <willedouglas10@gmail.com>
2020-07-27 14:28:11 -0300

    fix: updated origin file and env development

--- src/config/origin.js
+++ src/config/origin.js
@@ -7,8 +7,8 @@ dotenv();
 const getOriginUrl = () => {
   let originUrl = 'http://localhost:8081';
 
-  const isProduction = process.env.NODE_ENV = 'production';
-  const isStaging = process.env.NODE_ENV = 'staging';
+  const isProduction = process.env.NODE_ENV === 'production';
+  const isStaging = process.env.NODE_ENV === 'staging';
 
   if (isProduction) {
     originUrl = 'https://adfinance-register.a55.tech';
