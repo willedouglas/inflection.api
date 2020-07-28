64dfec1e314323a5da4153e19df7bd22501c7b28
Douglas Wille <willedouglas10@gmail.com>
2020-07-27 22:23:42 -0300

    fix: updated origin

--- src/config/origin.js
+++ src/config/origin.js
@@ -4,23 +4,7 @@ const dotenv = require('./dotenv');
 
 dotenv();
 
-const getOriginUrl = () => {
-  let originUrl = 'http://localhost:8081';
-
-  const isProduction = process.env.NODE_ENV === 'production';
-  const isStaging = process.env.NODE_ENV === 'staging';
-
-  if (isProduction) {
-    originUrl = 'https://adfinance-register.a55.tech';
-  }
-
-  if (isStaging) {
-    originUrl = 'https://adfinance-register-staging.a55.tech';
-  }
-
-  console.log(originUrl, process.env.NODE_ENV);
-  return originUrl;
-}
+const getOriginUrl = () => '*';
 
 const origin = {
   origin: getOriginUrl(),
