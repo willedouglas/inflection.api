d010e0ed75a2717e233556ca7adf1fff1dfb2dc1
Douglas Wille <willedouglas10@gmail.com>
2020-08-07 16:47:18 -0300

    fix: updated cors

--- src/config/origin.js
+++ src/config/origin.js
@@ -8,7 +8,7 @@ const isStaging = process.env.ENV === 'staging';
 const isProduction = process.env.ENV === 'production';
 
 const origin = {
-  origin: isProduction ? 'https://adfinance-api.a55.tech' : isStaging ? 'https://adfinance-api-staging.a55.tech' : 'http://localhost:8081',
+  origin: isProduction ? 'https://adfinance-register.a55.tech' : isStaging ? 'https://adfinance-register-staging.a55.tech' : 'http://localhost:8081',
 };
 
 module.exports = origin;
