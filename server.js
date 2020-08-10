5bf7aaf780dad529deb6642f20b051b2e3daf4ac
Douglas Wille <willedouglas10@gmail.com>
2020-08-10 11:40:10 -0300

    fix: adjusted wirecard keys

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const origin = require('./src/config/origin');
 const routes = require('./src/routes/index');
 const cors = require('cors');
 
@@ -9,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors(origin));
 
 routes(app);
 
--- src/config/wirecard.keys.js
+++ src/config/wirecard.keys.js
@@ -2,9 +2,9 @@ const isStaging = process.env.ENV === 'staging';
 const isProduction = process.env.ENV === 'production';
 
 module.exports = {
-  client_id: isProduction ? '' : isStaging ? 'APP-K3ME7DC6BMDC' : 'APP-SVS317VVB2IC',
-  client_secret: isProduction ? '' : isStaging ? 'b71ffcec3877423b827d325e0d794d14' : 'c3d66ebef7844030bda1b00ff10170ea',
-  redirectUri: isProduction ? '' : isStaging ? 'https://adfinance-register-staging.a55.tech/wirecard' : 'http://localhost:8081/wirecard',
-  accessToken: isProduction ? '' : isStaging ? '5817fadfcc67460aaddf69700afe1a90_v2' : '94dc79ab48344a9da6b8b3abe575b211_v2',
+  client_id: isProduction ? 'APP-K3ME7DC6BMDC' : isStaging ? 'APP-K3ME7DC6BMDC' : 'APP-SVS317VVB2IC',
+  client_secret: isProduction ? 'b71ffcec3877423b827d325e0d794d14' : isStaging ? 'b71ffcec3877423b827d325e0d794d14' : 'c3d66ebef7844030bda1b00ff10170ea',
+  redirectUri: isProduction ? 'https://adfinance-register.a55.tech/wirecard' : isStaging ? 'https://adfinance-register-staging.a55.tech/wirecard' : 'http://localhost:8081/wirecard',
+  accessToken: isProduction ? '5817fadfcc67460aaddf69700afe1a90_v2' : isStaging ? '5817fadfcc67460aaddf69700afe1a90_v2' : '94dc79ab48344a9da6b8b3abe575b211_v2',
   scopes: ['RETRIEVE_FINANCIAL_INFO'],
 };
