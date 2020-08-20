a7ad9a38b36b4dea8279069ea43bef582d2f2478
Douglas Wille <willedouglas10@gmail.com>
2020-08-20 05:00:43 -0300

    feat: updated cors, added whitelist with platform

--- src/config/origin.js
+++ src/config/origin.js
@@ -4,11 +4,23 @@ const dotenv = require('./dotenv');
 
 dotenv();
 
-const isStaging = process.env.ENV === 'staging';
-const isProduction = process.env.ENV === 'production';
+const whitelist = [
+  'https://adfinance-register.a55.tech',
+  'https://adfinance-register-staging.a55.tech',
+  'https://localhost:8081',
+  'https://plataforma.a55.local:8080',
+  'https://plataforma.a55.tech',
+  'https://plataforma-staging.a55.tech',
+];
 
 const origin = {
-  origin: isProduction ? 'https://adfinance-register.a55.tech' : isStaging ? 'https://adfinance-register-staging.a55.tech' : 'https://localhost:8081',
-};
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS.'))
+    }
+  }
+}
 
 module.exports = origin;
