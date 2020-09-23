1e60f6cde7e640583d0b685e41f12e661089d6ed
Eduardo Alencar <ealencar10@gmail.com>
2020-09-23 00:23:48 -0300

    feat: move bankly authentication to the middleware

---
+++ src/middlewares/bankly.js
@@ -0,0 +1,24 @@
+const { check, validationResult } = require('express-validator');
+const api = require('../helpers/api');
+
+const authentication = async(req, res, next) => {
+    
+    const authServerUrl = process.env.BANKLY_AUTH_SERVER_URL
+    const apiHelper = api({
+        headers: {
+            "content-type": "application/x-www-form-urlencoded"
+        }
+    })
+    const params = new URLSearchParams();
+    params.append('grant_type', 'client_credentials');
+    params.append('client_id', process.env.BANKLY_CLIENT_ID);
+    params.append('client_secret',process.env.BANKLY_CLIENT_SECRET);
+
+    const response = await apiHelper.post(authServerUrl, params);
+    req.token = response.data.access_token;	
+    next()
+}
+
+module.exports = {
+	authentication,
+}
