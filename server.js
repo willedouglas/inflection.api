93fbcbd6979604311794029fa08e4b3678919a9b
Felipe Carvalho <P5180242@brasilprev.com.br>
2020-10-26 19:02:49 -0300

    feat: adding sentry

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const Sentry = require('@sentry/node');
 const cors = require('cors');
 const cron = require('node-cron');
 const origin = require('./src/config/origin');
@@ -9,6 +9,17 @@ const routes = require('./src/routes/index');
 const jobs = require('./src/helpers/cronjobs');
 
 const app = express();
+
+Sentry.init({
+  attachStacktrace: true,
+  debug: true,
+  environment: process.env.NODE_ENV,
+  release: '0.0.1',
+  dsn:
+    process.env.NODE_ENV !== 'development'
+      ? process.env.ADSFINANCE_SENTRY_DSN
+      : process.env.ADSFINANCE_SENTRY_DSN,
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
