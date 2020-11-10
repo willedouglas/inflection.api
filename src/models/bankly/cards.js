b17e79ac9cab6ce8da41ad04dc6817377e84471c
Felipe Carvalho <P5180242@brasilprev.com.br>
2020-11-10 12:40:20 -0300

    feat: get virtual card

--- src/models/bankly/cards.js
+++ src/models/bankly/cards.js
@@ -1,5 +1,25 @@
 const pool = require('../../config/pool');
 
+const getCard = async ({
+  account_id,
+}) => {
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    const card = await client.query(`
+      SELECT proxy, activate_code, account_number, created_on
+      FROM adfinance.bankly_account
+      WHERE account_id = $1 ORDER BY id DESC LIMIT 1;`, [account_id]);
+    await client.query('COMMIT');
+    return card.rows[0];
+  } catch (e) {
+    await client.query('ROLLBACK');
+    throw e;
+  } finally {
+    client.release();
+  }
+};
+
 const insertVirtualCard = async ({
   account_id,
   proxy,
@@ -29,5 +49,6 @@ const insertVirtualCard = async ({
 };
 
 module.exports = {
+  getCard,
   insertVirtualCard,
 };
