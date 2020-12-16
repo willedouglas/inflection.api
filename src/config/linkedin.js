5995c6740c8ce60aba22ab9d792b27b72633e573
Douglas Wille <willedouglas10@gmail.com>
2020-12-16 00:23:57 -0300

    chore: updated redirect url when production env

--- src/config/linkedin.js
+++ src/config/linkedin.js
@@ -1,7 +1,12 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
+const PRODUCTION_REDIRECT_URI = 'https://inflection-web.herokuapp.com/auth/linkedin/callback';
+const STAGING_REDIRECT_URI = 'http://localhost:8080/auth/linkedin/callback';
+
 module.exports = {
+  redirect_uri: isProduction ? PRODUCTION_REDIRECT_URI : STAGING_REDIRECT_URI,
   auth_uri: 'https://www.linkedin.com',
   api_uri: 'https://api.linkedin.com',
-  redirect_uri: 'http://localhost:8080/auth/linkedin/callback',
   client_secret: '10gENh92YmP7XCQj',
   client_id: '77hn4xyq2grtuf',
 };
